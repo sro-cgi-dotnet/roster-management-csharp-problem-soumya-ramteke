4c71c28eb530ae77b286de2c3e5889b59b97f747
soumya-ramteke <soumya.r@cgi.com>
2018-09-15 19:18:06 +0530

    .net assgnmt 5

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -19,9 +19,43 @@ namespace RosterManagement
         /// <param name="cadet">Refers to the name of the Cadet</param>
         /// <param name="wave">Refers to the Wave number</param>
         public void Add(string cadet, int wave)
-        {
+        {   //first check whether the wave exists or not
+            if(_roster.ContainsKey(wave))
+            {   //add cadet to the particular wave
+                _roster[wave].Add(cadet);
+            }
+            else
+            {   //if the wave doesnt exist, create a new wave
+                _roster.Add(wave, new List<string>() );
+                //add cadet
+                _roster[wave].Add(cadet);
+            }
         }
-
+        /// <summary>
+        /// Return all the cadets in the CodeSchool
+        /// </summary>
+        /// <returns>List of Cadet's Name</returns>
+        public List<string> Roster()
+        {
+            var cadets = new List<string>();
+            //get all the waves, store them in a list
+            var waves= new List<int>();
+            waves=_roster.Keys.ToList();
+            //sort them
+            waves.Sort();
+            //loop through each wave
+            foreach(int w in waves)
+            {   //first sort names alphabetically
+                _roster[w].Sort();
+                //loop through to get the cadet names
+                foreach(string cadet_name in _roster[w])
+                {
+                    //add to cadets list
+                    cadets.Add(cadet_name);
+                }
+            }
+            return cadets;
+        }
         /// <summary>
         /// Returns all the Cadets in a given wave
         /// </summary>
@@ -30,17 +64,19 @@ namespace RosterManagement
         public List<string> Grade(int wave)
         {
             var list = new List<string>();
+            //find the wave
+            if(_roster.ContainsKey(wave))
+            {   //loop through the wave
+                foreach(string s in _roster[wave])
+                {   //add the cadet name
+                    list.Add(s);
+                }
+                //sort the list
+                list.Sort();
+            }
             return list;
-        }
-
-        /// <summary>
-        /// Return all the cadets in the CodeSchool
-        /// </summary>
-        /// <returns>List of Cadet's Name</returns>
-        public List<string> Roster()
-        {
-            var cadets = new List<string>();
-            return cadets;
-        }
-    }
+        }      
 }
+}
+
+
